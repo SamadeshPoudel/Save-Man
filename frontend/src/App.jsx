c3ea8a8b7afe65766df03f232d11972cb2e80994
Samadesh Poudel <poudelsamadesh@gmail.com>
2024-07-30 12:58:48 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center bg-slate-600 text-2xl font-semibold bg-blend-color-burn cursor-pointer mt-10 py-5'>Welcome to save man game</h1>
+    </>
+  )
+}
+
+export default App
